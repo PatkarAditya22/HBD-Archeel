ff214269c25f0aab9d086468c27e7dca2c28cda2
PatkarAditya22 <adityapatkar22@gmail.com>
2021-02-11 22:23:10 +0530

    changes

---
+++ Siddharth/archeel.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <conio.h>
+#include <dos.h>
+using namespace std;
+
+int main()
+{
+
+    int C = 523;
+    int  D = 587;
+    int E = 659;
+    int F = 698;
+    int G = 740;
+    int A = 880;
+    int B = 988;
+
+
+	char h = '&';
+
+	Sleep(3002);
+
+	system("cls");
+
+	Sleep(500);
+
+
+	for (int i = 0; i < 40; i++) {
+		cout << "_";
+	}
+	Sleep(500);
+
+
+	cout << endl << endl << "\t\t\t\t  i i i i i" << endl;
+	Sleep(500);
+	Beep(C, 382);
+	Beep(C, 382);
+	Beep(D, 340);
+	Beep(C, 382);
+	Beep(F, 286);
+	Beep(E, 304);
+	cout << "\t\t\t\t  i i i i i" << endl;
+	Sleep(500);
+
+	Beep(C, 382);
+	Beep(C, 382);
+	Beep(D, 340);
+	Beep(C, 382);
+	Beep(G, 270);
+	Beep(F, 286);
+
+	cout << "\t\t\t\t__i_i_i_i_i__";
+	Sleep(500);
+
+	Beep(C, 382);
+	Beep(C, 382);
+	Beep(C, 382);
+	Beep(A, 228);
+	Beep(F, 286);
+	Beep(E, 304);
+
+
+	cout << endl << "\t\t\t       |" << h << "\t    " << h << "|";
+	Sleep(500);
+	Beep(D, 340);
+	Beep(B, 220);
+	Beep(B, 220);
+	Beep(A, 228);
+	Beep(F, 286);
+	Beep(G, 290);
+	Beep(F, 286);
+	Beep(E, 304);
+
+
+	cout << endl << "\t\t\t     __| " << h << "         " << h << " |__";
+	Sleep(500);
+
+	Beep(C, 382);
+	Beep(C, 382);
+	Beep(D, 340);
+	Beep(C, 382);
+	Beep(F, 286);
+	Beep(E, 304);
+
+	cout << endl << "\t\t\t    | " << h << "    " << h << "     " << h << "    " << h << " |";
+	Sleep(500);
+
+	Beep(C, 382);
+	Beep(C, 382);
+	Beep(D, 340);
+	Beep(C, 382);
+	Beep(G, 270);
+	Beep(F, 286);
+
+	cout << endl << "\t\t\t  __|" << h << "   HAPPY   " << h << "     " << h << "|__";
+	Sleep(500);
+
+	Beep(C, 382);
+	Beep(C, 382);
+	Beep(C, 382);
+	Beep(A, 228);
+	Beep(F, 286);
+	Beep(E, 304);
+
+	cout << endl << "\t\t\t | " << h << "  BIRTHDAY    " << h << "      " << h << " |";
+	Sleep(500);
+    Beep(D, 340);
+	Beep(B, 220);
+	Beep(B, 220);
+	Beep(A, 228);
+	Beep(F, 286);
+	Beep(G, 290);
+
+	cout << endl << "\t\t\t |" << h << " ARCHEEL   " << "   " << h << "  " << h << "  " << h << "  " << h << "|";
+	Sleep(500);
+	//Beep(523, 500);
+	cout << endl << "\t\t\t | " << h << "  " << h << "            " << h << "     " << h << " |";
+	Sleep(500);
+	//Beep(523, 500);
+	cout << endl << "\t\t\t |";
+	for (int i = 0; i < 25; i++) {
+		cout << "_";
+	}
+	cout << "|" << endl<<endl<<endl;
+
+	cout << "\t\tHAPPY BIRTHDAY Jugadu";
+	cout << endl << "\t\tHaar saal kya pel raha h bol bol  kar hame hi pel deta h"<<endl<<endl;
+	cout << endl << "\t\tAll the best age ke liye aur ho sake to hame yaad rakhna bhale hi itne memories nahi h."<<endl<<endl;
+	cout<<"\t\t<ya mujhe yaad nahi h,not good with memories :)>"<<endl<<endl;
+	cout << endl << "\t\tAur ek sifarish h ki NETFLIX ka jugaad fir se chalu kardena aur khud ka assignment bhi kabhi dena "<<endl<<endl;
+	cout<<"\t\t Bas itna hi kar paya C++ mai"<<endl<<endl;
+
+
+}
